30d58dc84cabb577b01299eaf0c5fab2626e28ed
クリフロ-Crystal Frontier- <makinaharyu@gmail.com>
2026-02-27 23:13:43 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -809,7 +809,7 @@ class UIManager {
         // ★変更1：余白を少し戻して（ズームを少し大きくして）画面にピッタリ合わせます
         let minScale = Math.min(scaleX, scaleY); 
         if (document.body.classList.contains('is-pc')) {
-            minScale *= 0.90; // PCは元々の丁度よかったサイズに戻します
+            minScale *= 0.98; // ★ここの数字を「0.90」から「0.98」に差し替えました！
         } else {
             minScale *= 0.82; // スマホは小さくしすぎたので、見切れないギリギリのサイズに調整します！
         }
