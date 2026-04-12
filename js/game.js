d6b8a0e24399559ec53b2105e5d65540bf2aa1e4
クリフロ-Crystal Frontier- <makinaharyu@gmail.com>
2026-04-13 02:58:28 +0900

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -450,16 +450,16 @@ class GameSystem {
     static toGradeHTML(val) {
         let base = "", sub = "", cls = "";
         if (val >= 96) { base = "S"; sub = "1"; cls = "rank-s"; } 
-        else if (val >= 91) { base = "S"; sub = "2"; cls = "rank-s"; }
-        else if (val >= 81) { base = "A"; sub = "1"; cls = "rank-a"; } 
-        else if (val >= 76) { base = "A"; sub = "2"; cls = "rank-a"; }
-        else if (val >= 66) { base = "B"; sub = "1"; cls = "rank-b"; } 
-        else if (val >= 61) { base = "B"; sub = "2"; cls = "rank-b"; }
-        else if (val >= 51) { base = "C"; sub = "1"; cls = "rank-c"; } 
-        else if (val >= 46) { base = "C"; sub = "2"; cls = "rank-c"; }
-        else if (val >= 36) { base = "D"; sub = "1"; cls = "rank-d"; } 
-        else if (val >= 31) { base = "D"; sub = "2"; cls = "rank-d"; }
-        else if (val >= 21) { base = "E"; sub = "1"; cls = "rank-e"; } 
+        else if (val >= 91) { base = "S"; sub = "";  cls = "rank-s"; }
+        else if (val >= 81) { base = "A"; sub = "+"; cls = "rank-a"; } 
+        else if (val >= 76) { base = "A"; sub = "";  cls = "rank-a"; }
+        else if (val >= 66) { base = "B"; sub = "+"; cls = "rank-b"; } 
+        else if (val >= 61) { base = "B"; sub = "";  cls = "rank-b"; }
+        else if (val >= 51) { base = "C"; sub = "+"; cls = "rank-c"; } 
+        else if (val >= 46) { base = "C"; sub = "";  cls = "rank-c"; }
+        else if (val >= 36) { base = "D"; sub = "+"; cls = "rank-d"; } 
+        else if (val >= 31) { base = "D"; sub = "";  cls = "rank-d"; }
+        else if (val >= 21) { base = "E"; sub = "+"; cls = "rank-e"; } 
         else { base = "E"; sub = "2"; cls = "rank-e"; }
 
         return `
