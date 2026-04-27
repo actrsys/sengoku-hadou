340a439fbab611506eeefc9936b1f4600b0ef85b
クリフロ-Crystal Frontier- <makinaharyu@gmail.com>
2026-04-28 05:13:14 +0900

    Add files via upload

--- js/ui_delegate.js
+++ js/ui_delegate.js
@@ -31,23 +31,28 @@ class UIDelegateManager {
         const daimyo = this.game.bushos.find(b => b.clan === this.game.playerClanId && b.isDaimyo);
         const daimyoCastleId = daimyo ? daimyo.castleId : -1;
         const myCastles = this.game.castles.filter(c => c.ownerClan === this.game.playerClanId && c.id !== daimyoCastleId);
+        
+        const headerContainer = document.getElementById('delegate-header-area');
+        if (headerContainer) {
+            headerContainer.innerHTML = `
+                <div class="delegate-bulk-row">
+                    <span class="col-castle-name"></span>
+                    <span class="col-castellan"></span>
+                    <span class="col-attack"><button class="delegate-header-btn" onclick="window.GameApp.ui.delegate.toggleAllAttack()">一括</button></span>
+                    <span class="col-move"><button class="delegate-header-btn" onclick="window.GameApp.ui.delegate.toggleAllMove()">一括</button></span>
+                    <span class="col-status"><button class="delegate-header-btn" onclick="window.GameApp.ui.delegate.toggleAllStatus()">一括</button></span>
+                </div>
+                <div class="list-header delegate-list-header">
+                    <span class="col-castle-name">拠点名</span>
+                    <span class="col-castellan">城主</span>
+                    <span class="col-attack">城攻め</span>
+                    <span class="col-move">武将移動</span>
+                    <span class="col-status">状態</span>
+                </div>
+            `;
+        }
 
-        let html = `
-            <div class="delegate-bulk-row">
-                <span class="col-castle-name"></span>
-                <span class="col-castellan"></span>
-                <span class="col-attack"><button class="delegate-header-btn" onclick="window.GameApp.ui.delegate.toggleAllAttack()">一括</button></span>
-                <span class="col-move"><button class="delegate-header-btn" onclick="window.GameApp.ui.delegate.toggleAllMove()">一括</button></span>
-                <span class="col-status"><button class="delegate-header-btn" onclick="window.GameApp.ui.delegate.toggleAllStatus()">一括</button></span>
-            </div>
-            <div class="list-header delegate-list-header">
-                <span class="col-castle-name">拠点名</span>
-                <span class="col-castellan">城主</span>
-                <span class="col-attack">城攻め</span>
-                <span class="col-move">武将移動</span>
-                <span class="col-status">状態</span>
-            </div>
-        `;
+        let listHtml = "";
 
         myCastles.forEach(c => {
             const hasCastellan = c.castellanId && c.castellanId > 0;
@@ -66,7 +71,7 @@ class UIDelegateManager {
             let moveText = c.allowMove ? "許可" : "不可";
             let moveDisabled = !c.isDelegated ? "disabled" : "";
 
-            html += `
+            listHtml += `
                 <div class="select-item delegate-list-item" style="cursor:default;">
                     <span class="col-castle-name">${c.name}</span>
                     <span class="col-castellan">${castellanName}</span>
@@ -83,19 +88,7 @@ class UIDelegateManager {
             `;
         });
 
-        for (let i = myCastles.length; i < 7; i++) {
-            html += `
-                <div class="select-item delegate-list-item empty-row" style="cursor:default;">
-                    <span class="col-castle-name"></span>
-                    <span class="col-castellan"></span>
-                    <span class="col-attack"></span>
-                    <span class="col-move"></span>
-                    <span class="col-status"></span>
-                </div>
-            `;
-        }
-
-        listContainer.innerHTML = `<div class="list-inner-wrapper" style="width: 100%; min-width: 100%;">${html}</div>`;
+        listContainer.innerHTML = `<div class="list-inner-wrapper" style="width: 100%; min-width: 100%;">${listHtml}</div>`;
     }
 
     toggleAllStatus() {
