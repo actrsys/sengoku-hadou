45812bd2b393b6f0b1b1f942256166d2f150cf14
クリフロ-Crystal Frontier- <makinaharyu@gmail.com>
2026-06-13 23:52:17 +0900

    Add files via upload

--- js/life_system.js
+++ js/life_system.js
@@ -438,8 +438,8 @@ class LifeSystem {
                 // もしプレイヤーの家臣で、すでに登場していたらお知らせを出します
                 if (b.clan === this.game.playerClanId && !wasUnborn) {
                     const name = b.name.replace('|', '');
-                    this.game.ui.log(`野戦での傷が元で、${name}が死亡しました……`);
-                    await this.game.ui.showDialogAsync(`野戦での傷が元で、${name}が死亡しました……`, false, 0);
+                    this.game.ui.log(`野傷が元となり、${name}が死亡しました……`);
+                    await this.game.ui.showDialogAsync(`野傷が元となり、${name}が死亡しました……`, false, 0);
                 }
             }
         }
